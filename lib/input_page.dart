ad64d7de21a91ee9e98ed15881035a0d6f3f3ec5
Jakub Zaremba <jzaremba@MAgiC.local>
2021-07-06 23:16:28 +0200

    Learning about gestureDetector Widget and ternary operator

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,6 +5,7 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -13,66 +14,103 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  
+
+  Color maleCardColor = inactiveCardColor;
+  Color femaleCardColor = inactiveCardColor;
+
+  // 1 - male, 2 - female
+  void updateColor(int gender){
+    if(gender==1){
+      maleCardColor == inactiveCardColor ? maleCardColor = activeCardColor : maleCardColor = inactiveCardColor;
+      if(femaleCardColor == activeCardColor) femaleCardColor = inactiveCardColor;
+    } else if(gender == 2){
+      femaleCardColor == inactiveCardColor ? femaleCardColor = activeCardColor : femaleCardColor = inactiveCardColor;
+      if(maleCardColor == activeCardColor)  maleCardColor = inactiveCardColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body:
-           Column(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
               children: <Widget>[
                 Expanded(
                   flex: 1,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          flex: 1,
-                          child: ReusableCard(color: activeCardColor,
-                          cardChild: IconContent(icon: FontAwesomeIcons.mars,
-                            label: "MALE",
-                          ),
-                          ),
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
+                    child: ReusableCard(
+                      color: maleCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: "MALE",
                       ),
-                      Expanded(
-                          flex: 1,
-                          child: ReusableCard(color: activeCardColor,
-                          cardChild: IconContent(icon: FontAwesomeIcons.venus,
-                            label: "FEMALE",
-                          )
-                            ,),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-                Expanded(
-                    flex: 1,
-                    child: ReusableCard(color: activeCardColor,),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                          flex: 1,
-                          child: ReusableCard(color: activeCardColor,),
+                  child: GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(2);
+                      });
+                    },
+                    child: ReusableCard(
+                      color: femaleCardColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: "FEMALE",
                       ),
-                      Expanded(
-                          flex: 1,
-                          child: ReusableCard(color: activeCardColor,),
-                      ),
-                    ],
+                    ),
                   ),
                 ),
-              Container(
-                color: bottomContainerColor,
-                margin: EdgeInsets.only(top: 10.0),
-                width: double.infinity,
-                height: bottomContainerHeight,
-              )
               ],
             ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ReusableCard(
+              color: activeCardColor,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(
+                    color: activeCardColor,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(
+                    color: activeCardColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      ),
     );
   }
 }
